ef689ffb67634f58b887e8c8e74003867336e402
Omer Gronich <gronichomer@gmail.com>
2022-07-17 23:51:15 +0300

    created let directive

---
+++ libs/utils/src/lib/utils.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LetDirective } from './let.directive';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [
+    LetDirective
+  ],
+})
+export class UtilsModule {}
