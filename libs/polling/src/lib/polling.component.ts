da922ece14810f069b878ebce123ceb064998433
Omer Gronich <gronichomer@gmail.com>
2022-07-17 23:50:43 +0300

    created let directive

--- libs/polling/src/lib/polling.component.ts
+++ libs/polling/src/lib/polling.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'rx-use-cases-polling',
@@ -7,4 +7,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PollingComponent {
+  startPolling() {
+
+  }
 }
