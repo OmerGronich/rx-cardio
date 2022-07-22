0c3b9298e98d46293ccdb97899d5b038232b6c9e
Omer Gronich <gronichomer@gmail.com>
2022-07-23 00:34:58 +0300

    improved types

--- libs/polling/src/lib/map-status-to-color.pipe.ts
+++ libs/polling/src/lib/map-status-to-color.pipe.ts
@@ -1,16 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+const _statusToColor = {
+  pending: 'accent',
+  success: 'primary',
+  error: 'warn',
+} as const;
+type StatusToColor = typeof _statusToColor;
+type StatusToColorKeys = keyof StatusToColor;
+
 @Pipe({
-  name: 'mapStatusToColor'
+  name: 'mapStatusToColor',
 })
 export class MapStatusToColorPipe implements PipeTransform {
-
-  transform(value: string): unknown {
-    return {
-      pending: 'accent',
-      success: 'primary',
-      error: 'warn'
-    }[value];
+  transform(value: StatusToColorKeys): StatusToColor[StatusToColorKeys] {
+    return _statusToColor[value];
   }
-
 }
